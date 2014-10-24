5c292455a42be8df2e3be9b361ea4bd675318578
Mike Blacker <uncmike2005@gmail.com>
2014-10-24 17:15:43 +0100

    added userdata and setup configs for saltmaster on aws

--- masterUserData/userdata.bash
+++ masterUserData/userdata.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
+git clone git@github.com:uncmike2005/saltstack.git /root/saltstack
+/root/saltstack/setup.bash
 
-rpm -Uvh http://ftp.linux.ncsu.edu/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-yum -y install salt-master git
-git clone git@github.com:uncmike2005/saltstack.git
---
+++ setup.bash
@@ -0,0 +1,49 @@
+#start the salt maste
+rpm -Uvh http://ftp.linux.ncsu.edu/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum -y install git salt-master jq
+service salt-master start
+
+##stuff to do
+
+##set up aws cli
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+pip install awscli
+mkdir ~/.aws
+wget -O mycreds -q 'http://169.254.169.254/latest/meta-data/iam/security-credentials/saltProvisioner'
+export AWS_SECRET_ACCESS_KEY=`jq -r '.SecretAccessKey' <mycreds`
+export AWS_ACCESS_KEY_ID=`jq -r '.AccessKeyId' <mycreds`
+export AWS_SESSION_TOKEN=`jq -r '.Token' <mycreds`
+export AWS_DEFAULT_REGION='eu-west-1'
+
+##generate key and push to aws
+ssh-keygen -f /etc/salt/my_salt_cloud_key -t rsa -b 4096
+aws ec2 delete-key-pair --key-name salt_cloud_key
+aws ec2 import-key-pair --key-name salt_cloud_key --public-key-material file:///etc/salt/my_salt_cloud_key.pub
+
+##set up security groups
+aws ec2 delete-security-group --group-name MySecurityGroupSaltCloudInstances
+aws ec2 create-security-group \
+    --group-name MySecurityGroupSaltCloudInstances \
+    --description "The Security Group applied to all salt-cloud instances"
+aws ec2 authorize-security-group-ingress \
+    --group-name MySecurityGroupSaltCloudInstances \
+    --source-group MySecurityGroupSaltCloud \
+    --protocol tcp --port all
+aws ec2 authorize-security-group-egress \
+    --group-name MySecurityGroupSaltCloudInstances \
+    --source-group MySecurityGroupSaltCloud \
+    --protocol tcp --port all
+aws ec2 authorize-secrrity-group-egress \
+    --group-name MySecurityGroupSaltCloudInstances \
+    --source-group MySecurityGroupSaltCloud \
+    --protocol tcp --port all --cidr 0.0.0.0/0
+aws ec2 authorize-secrrity-group-egress \
+    --group-name MySecurityGroupSaltCloudInstances \
+    --source-group MySecurityGroupSaltCloud \
+    --protocol udp --port all --cidr 0.0.0.0/0
+
+
+##set up cloud.providers and cloud.profile
+
+
